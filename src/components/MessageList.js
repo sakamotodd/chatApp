88757733914f3b72cda89cacdcb8da57f11a3fa5
坂本大五郎 <skmtdgr@daigoro.local>
2020-12-17 12:21:39 +0900

    feth-messages

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import { makeStyles } from "@material-ui/core/styles";
-const useStyle =  makeStyles({
+import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { messagesRef } from '../firebase';
+
+const useStyle = makeStyles({
   root: {
     gridRow: 1,
   },
 });
 
 const MessageList = () => {
+  const [messages, setMessages] = useState([]);
   const classes = useStyle();
-  return <div className={classes.root}>MessageList</div>
+ useEffect(() => {
+   console.log('useEffect');
+  messagesRef
+  .orderByKey()
+  .limitToLast(3)
+  .on('value', (snapshot) => {
+    const messages = snapshot.val();
+    if(messages === null) return;
+    const entries = Object.entries(messages);
+    const newMessages = entries.map((entry) => {
+      const [key, nameAndText] = entry;
+      // const key = entry[0];
+      // const nameAndText = entry[1];
+      return { key, ...nameAndText };
+    });
+    console.log(newMessages);
+    setMessages(newMessages);
+  });
+ }, []);
+  return <div className={classes.root}>MessageList</div>;
 };
-
 
 export default MessageList;
