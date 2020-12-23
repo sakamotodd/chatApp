3a78cb9f9ad397cf2f8a2dd0102f6e4ad7b791bf
坂本大五郎 <skmtdgr@daigoro.local>
2020-12-24 06:42:39 +0900

    create-env-file

---
+++ .firebase-config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    apiKey: "AIzaSyAMXnVjbVvH-mRThVHPrphsNMMCnk47NIY",
+    authDomain: "chatapp-bd5bb.firebaseapp.com",
+    databaseURL: "https://chatapp-bd5bb-default-rtdb.firebaseio.com",
+    projectId: "chatapp-bd5bb",
+    storageBucket: "chatapp-bd5bb.appspot.com",
+    messagingSenderId: "666759943692",
+    appId: "1:666759943692:web:623ddd316263d4a44d4029",
+    measurementId: "G-3Z43J6BMZW"
+  };
